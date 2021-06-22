75c36ab8794a79ab04de51cbeec364e2b9652952
Sharmila <sharmilack1994@gmail.com>
2021-06-22 14:39:00 +0200

    indentation

--- Frontend/confirmation.js
+++ Frontend/confirmation.js
@@ -1,6 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const orderId = urlParams.get('orderId');
+
 document.getElementById("mainOrder").innerHTML = 'Your order was successful. the unique id of your order is ' + orderId; 
+
 
 
 
