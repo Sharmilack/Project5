1260c016ae5e469b001b9aa20b5a9e795cc13b57
Sharmila <sharmilack1994@gmail.com>
2021-07-03 09:43:27 +0200

    localstorage quantity

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 fetch('http://localhost:3000/api/cameras')
 	.then(response =>response.json())
     .then(data => {
-    	console.log("fetch succeeded");
     	var output = '' ;
         data.forEach(camera => {
         output +=`    
--- product.js
+++ product.js
@@ -23,25 +23,39 @@ fetch('http://localhost:3000/api/cameras/' + productId)
 	   		`;
 
 	document.getElementById('maincontainer').innerHTML=output;
+
+	const addCart=document.getElementById('addToCart')
     
     document.getElementById('addToCart').addEventListener('click', ()=>{
     	addToCart.style.color ="#7cb9c4";
-	
+ 
 		cartItems = localStorage.getItem('cart');
-		console.log(cartItems);
-		if (cartItems == null){
-			cartItems = [];
-			cartItems.push(productId);
+			let items = {id:camera._id, image:camera.imageUrl, name:camera.name, prices:camera.price/100, quantity:1};
+			if (cartItems === null){
+				cartItems = [];
+				cartItems.push(items);
+				localStorage.setItem('cart', JSON.stringify(cartItems));
+				console.log(cartItems);
+			} else {
+				cartItems=JSON.parse(cartItems);
+				cartItems.forEach (item => {
+					if (item.id === camera._id){
+						items.quantity = item.quantity += 1;
+						items.prices = item.prices*item.quantity;				
+				}else{
+				cartItems.push(items);
+				}
 			localStorage.setItem('cart', JSON.stringify(cartItems));
-		} else {
+
+
+		/*else {
 			cartItems = JSON.parse(cartItems);
 			if (!cartItems.includes(productId)){
 				cartItems.push(productId);
 				localStorage.setItem('cart', JSON.stringify(cartItems));
-			} else {
-				alert('le produit est déja dans le panier!')
 			}
-		}
-		console.log(cartItems);
-		}); 
+		}*/
 	});
+};
+});
+});
