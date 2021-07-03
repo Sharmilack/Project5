b0e8d029f60ec95c5dd72fc14af4d1e341608689
Dalou <autrusseau.damien@gmail.com>
2021-07-03 08:47:48 +0000

    Add an item and increment the quantity or clean existing carts

--- product.js
+++ product.js
@@ -1,16 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get('id');
+const productId = urlParams.get("id");
 
-fetch('http://localhost:3000/api/cameras/' + productId)
-	.then(response =>response.json())
-        .then(camera => {
-        	var output = '' ;
-        	output =` 
+fetch("http://localhost:3000/api/cameras/" + productId)
+    .then((response) => response.json())
+    .then((camera) => {
+        var output = "";
+        output = ` 
 			    <h2>${camera.name}</h2>
 			    <div class="productDetails">
 				    <div id="left">
 					 	<img src = ${camera.imageUrl}></img>
-					 	<p>Prix : ${camera.price/100} €</p>
+					 	<p>Prix : ${camera.price / 100} €</p>
 				    </div>
 			        <div id="right">
 			         	<p>${camera.description}</p>
@@ -22,40 +22,47 @@ fetch('http://localhost:3000/api/cameras/' + productId)
 			    <button class="button" id="addToCart">Ajouter au panier</button>
 	   		`;
 
-	document.getElementById('maincontainer').innerHTML=output;
+        document.getElementById("maincontainer").innerHTML = output;
 
-	const addCart=document.getElementById('addToCart')
-    
-    document.getElementById('addToCart').addEventListener('click', ()=>{
-    	addToCart.style.color ="#7cb9c4";
- 
-		cartItems = localStorage.getItem('cart');
-			let items = {id:camera._id, image:camera.imageUrl, name:camera.name, prices:camera.price/100, quantity:1};
-			if (cartItems === null){
-				cartItems = [];
-				cartItems.push(items);
-				localStorage.setItem('cart', JSON.stringify(cartItems));
-				console.log(cartItems);
-			} else {
-				cartItems=JSON.parse(cartItems);
-				cartItems.forEach (item => {
-					if (item.id === camera._id){
-						items.quantity = item.quantity += 1;
-						items.prices = item.prices*item.quantity;				
-				}else{
-				cartItems.push(items);
-				}
-			localStorage.setItem('cart', JSON.stringify(cartItems));
+        const addCart = document.getElementById("addToCart");
 
+        document.getElementById("addToCart").addEventListener("click", () => {
+            addToCart.style.color = "#7cb9c4";
 
-		/*else {
-			cartItems = JSON.parse(cartItems);
-			if (!cartItems.includes(productId)){
-				cartItems.push(productId);
-				localStorage.setItem('cart', JSON.stringify(cartItems));
-			}
-		}*/
-	});
-};
-});
-});
+            let cartItems = localStorage.getItem("cart");
+            // Si le panier n'a jamais existé ou est vide on l'initialise sinon on le parse
+            cartItems =
+                cartItems === null || cartItems == ""
+                    ? []
+                    : JSON.parse(cartItems);
+
+            // On cherche les items dans le panier qui ont le même id
+            let existingItems = cartItems.filter(
+                (item) => item.id == camera._id
+            );
+
+            // S'il y en au moins 1, on incrémente juste la quantité de celui ci
+            if (existingItems.length >= 1) {
+                existingItems[0].quantity += 1;
+                // S'il y en plus que 1, il y a problème, on clean les autres items du tableau
+                if (existingItems.length > 1) {
+                    cartItems = cartItems.filter(
+                        (item) => item.id != camera._id
+                    );
+                    cartItems.push(existingItems[0]);
+                }
+            }
+            // Sinon on ajoute un nouvel item
+            else {
+                cartItems.push({
+                    id: camera._id,
+                    image: camera.imageUrl,
+                    name: camera.name,
+                    prices: camera.price / 100,
+                    quantity: 1,
+                });
+            }
+            // Puis on serialise le tableau
+            localStorage.setItem("cart", JSON.stringify(cartItems));
+        });
+    });
